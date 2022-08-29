5b1bbaee7b2aa04a1687052ac041554e9ee1c249
Abdelwahab <abdel.sako@yahoo.com>
2022-08-28 22:38:40 -0500

    had to make a new local clone

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+squares are changing color
 # etch-a-sketch
 finally got the greid sizing right
--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 
 window.onload = function mkGrid(size) {
-    size = prompt('enter size')
+    size = 256
     for (let i = 0; i < size; i++) {
         const board = document.getElementById("board")
-        const div = document.createElement("div")
+        let div = document.createElement("div")
         div.className = "squares"
         board.appendChild(div)
     }
+   
+    
 }
 
-
+const board = document.getElementById("board")
+board.addEventListener("mousemove", function mousemove(event) {
+    console.log("color should change")
+    event.target.style.backgroundColor = "orange"
+   
+})
